312dbe76524e6858bac03240d32b58acad388cf3
mo <dayoujianghong@gmail.com>
2023-04-24 14:57:38 +0900

    edit 110_props_children/start/*

--- 04_react_basic/src/110_props_children/start/Example.js
+++ 04_react_basic/src/110_props_children/start/Example.js
@@ -1,17 +1,25 @@
-import Profile from "./components/Profile";
 import Container from "./components/Container";
+import Profile from "./components/Profile";
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+    {name: "Takashi", age: 19, country: "Japan"},
+    {name: "Jane", age: 28, country: "UK", color: "red"},
 ];
 
 const Example = () => {
-  return (
-    <div>
-      <Container title="Childrenとは？" />
-    </div>
-  );
+    return (
+        <div>
+            <Container title="Childrenとは？" children={
+                [
+                    <Profile key={profile[0].name} {...profile[0]} />,
+                    <Profile key={profile[1].name} {...profile[1]} />
+                ]
+            }
+                       first={<Profile key={profile[0].name} {...profile[0]} />}
+                       second={<Profile key={profile[1].name} {...profile[1]} />}
+            />
+        </div>
+    );
 };
 
 export default Example;
--- 04_react_basic/src/110_props_children/start/components/Container.js
+++ 04_react_basic/src/110_props_children/start/components/Container.js
@@ -1,11 +1,14 @@
 import "./Container.css";
 
-const Container = ({ title }) => {
-  return (
-    <div className="container">
-      <h3>{title}</h3>
-    </div>
-  );
+const Container = ({title, children, first, second}) => {
+    console.log(children);
+    return (
+        <div className="container">
+            <h3>{title}</h3>
+            {first}
+            {second}
+        </div>
+    );
 };
 
 export default Container;

79f92cbfa3fba9fd068db283fce9a9797cf44ea1
mo <dayoujianghong@gmail.com>
2023-04-25 15:06:36 +0900

    edit 010_eventlistener/start/Example.js

--- 05_state_and_event/src/010_eventlistener/start/Example.js
+++ 05_state_and_event/src/010_eventlistener/start/Example.js
@@ -1,7 +1,18 @@
 const Example = () => {
-  return (
-    <></>
-  );
+    const clickHandler = () => {
+        alert('ボタンがクリックされました。');
+    }
+    const clickHandler2 = () => {
+        console.log('ボタンがクリックされました。');
+    }
+    const hello = () => 'hello react';
+    return (
+        <>
+            <button onClick={clickHandler}>クリックしてね</button>
+            <button onClick={clickHandler2}>クリックしてね</button>
+            {hello}
+        </>
+    );
 };
 
 export default Example;

529c8318a029e51062821413457ac478a7a825de
mo <dayoujianghong@gmail.com>
2023-04-27 15:10:43 +0900

    edit 010_list_components/start/Example.js

--- 06_control_and_form/src/010_list_components/start/Example.js
+++ 06_control_and_form/src/010_list_components/start/Example.js
@@ -1,16 +1,26 @@
-
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
 
-  return (
-    <>
-      <h3>配列の操作</h3>
-      <ul>
-        
-      </ul>
-    </>
-  );
+    // const animalList = [];
+    // for (const animal of animals) {
+    //     animalList.push(<li>{animal}</li>);
+    // }
+
+    const helloAnimals = animals.map(animal => <li>Hello, {animal}</li>)
+
+    return (
+        <>
+            <h3>配列の操作</h3>
+            <ul>
+                {/*<li>{animals[0]}</li>*/}
+                {/*<li>{animals[1]}</li>*/}
+                {/*<li>{animals[2]}</li>*/}
+                {/*{animalList}*/}
+                {helloAnimals}
+            </ul>
+        </>
+    );
 };
 
 export default Example;

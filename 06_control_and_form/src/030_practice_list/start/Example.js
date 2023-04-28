25ab607b6be86cc71f5d563faa1fb08faec55b7c
mo <dayoujianghong@gmail.com>
2023-04-28 09:43:02 +0900

    edit 030_practice_list/start/*

--- 06_control_and_form/src/030_practice_list/start/Example.js
+++ 06_control_and_form/src/030_practice_list/start/Example.js
@@ -1,14 +1,49 @@
+import Profile from "./components/Profile";
+
+const profiles = [
+    {
+        name: "Geo",
+        age: 18,
+        hobbies: [
+            "sports",
+            "music",
+        ],
+    },
+    {
+        name: "Tom",
+        age: 25,
+        hobbies: [
+            "movie",
+            "music",
+        ],
+    },
+    {
+        name: "Lisa",
+        age: 21,
+        hobbies: [
+            "sports",
+            "travel",
+            "game",
+        ],
+    },
+]
+
 const Example = () => {
-  return (
-    <>
-      <h3>練習問題</h3>
-      <p>Profileコンポーネントを使用して、完成コードと同じ画面を作成してください。</p>
-      <p>また、Profileコンポーネント内のリスト表示部分にkeyを設定して、ワーニング表示がされないようにしてください。</p>
-      <ul>
-        {/* ここに記述 */}
-      </ul>
-    </>
-  );
+    return (
+        <>
+            <h3>練習問題</h3>
+            <p>Profileコンポーネントを使用して、完成コードと同じ画面を作成してください。</p>
+            <p>また、Profileコンポーネント内のリスト表示部分にkeyを設定して、ワーニング表示がされないようにしてください。</p>
+            <ul>
+                {profiles.map((profile) => (
+                    /* リストにはkeyを設定することを忘れないように！ */
+                    <li key={profile.name}>
+                        <Profile {...profile} />
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 };
 
 export default Example;

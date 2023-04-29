713cf1c3baa7d5962495a203a2edc9757d9cdf3d
mo <dayoujianghong@gmail.com>
2023-04-29 17:34:07 +0900

    edit 100_single_checkbox/start/Example.js

--- 06_control_and_form/src/100_single_checkbox/start/Example.js
+++ 06_control_and_form/src/100_single_checkbox/start/Example.js
@@ -1,11 +1,31 @@
+import {useState} from "react";
+
 const Example = () => {
-  return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
-  );
+    const [isChecked, setIsChecked] = useState(true);
+
+    const toggleChecked = (e) => {
+        setIsChecked((prevState) => !prevState);
+    };
+
+    return (
+        <div>
+            <p style={{textAlign: "center"}}>
+                startフォルダの内容が表示されます。
+                <br/>
+                練習用に使ってください！
+            </p>
+            <label htmlFor="my-check">
+                チェック:
+            </label>
+            <input
+                type="checkbox"
+                id="my-check"
+                checked={isChecked}
+                onChange={() => toggleChecked()}
+            />
+            <div>{isChecked ? "ON" : "OFF"}</div>
+        </div>
+    );
 };
 
 export default Example;

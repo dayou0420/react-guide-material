55e76dbeda3668a4c9b4338a50f5f62aead049b1
mo <dayoujianghong@gmail.com>
2023-04-29 19:12:53 +0900

    edit 110_multi_checkbox/start/Example.js

--- 06_control_and_form/src/110_multi_checkbox/start/Example.js
+++ 06_control_and_form/src/110_multi_checkbox/start/Example.js
@@ -1,20 +1,37 @@
-import { useState } from "react";
+import {useState} from "react";
 
 const Example = () => {
-  const [fruits, setFruits] = useState([
-    { label: "Apple", value: 100, checked: false },
-    { label: "Banana", value: 200, checked: false },
-    { label: "Cherry", value: 300, checked: false },
-  ]);
+    const [fruits, setFruits] = useState([
+        {label: "Apple", value: 100, checked: false},
+        {label: "Banana", value: 200, checked: false},
+        {label: "Cherry", value: 300, checked: false},
+    ]);
 
-  const [sum, setSum] = useState(0);
+    const [sum, setSum] = useState(0);
+    const handleChange = (e) => {
 
-  return (
-    <div>
-      
-      <div>合計：{sum}</div>
-    </div>
-  );
+    }
+    return (
+        <div>
+            {fruits.map((fruit) => {
+                return (
+                    <div key={fruit.label}>
+                        <input
+                            id={fruit.label}
+                            type="checkbox"
+                            value={fruit.label}
+                            checked={fruit.checked}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor={fruit.label}>
+                            {fruit.label}:{fruit.value}
+                        </label>
+                    </div>
+                );
+            })}
+            <div>合計：{sum}</div>
+        </div>
+    );
 };
 
 export default Example;
